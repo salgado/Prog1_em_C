7a6814e104a9ab0d9f5f33076d6a4c42e0e9f40f
Alex Salgado <alexsalgado1@gmail.com>
2015-05-19 22:27:14 -0300

    teste

---
+++ semana11/quadrado.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x = 4;
+    x = x ** 2;
+    
+    printf("x ao quadrado = %i\n", x);
+    
+    system("pause");
+    
+    return 0;
+}
